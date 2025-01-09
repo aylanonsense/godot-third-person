765baa5498a284b29521f5ab5920c427903d1745
Ayla Myers <4943384+aylanonsense@users.noreply.github.com>
2025-01-09 00:03:33 -0500

    Most basic third person controller

---
+++ main.gd
@@ -0,0 +1,13 @@
+class_name Main
+extends Node
+
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("toggle_mouse_lock"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED else Input.MOUSE_MODE_CAPTURED
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
---
+++ player_character.gd
@@ -0,0 +1,27 @@
+class_name PlayerCharacter
+extends CharacterBody3D
+
+
+@export var move_speed := 5.0
+@export var mouse_look_sensitivity := 1.0
+
+@onready var look_pivot := %LookPivot as Node3D
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+		var look_change := Vector2(event.relative.x, -event.relative.y) * mouse_look_sensitivity
+		look_pivot.rotation_degrees.x = clampf(look_pivot.rotation_degrees.x + look_change.y, -89.9, 89.9)
+		look_pivot.rotation_degrees.y -= look_change.x
+
+
+func _physics_process(_delta: float) -> void:
+	var move_vector := Input.get_vector("move_left", "move_right", "move_backward", "move_forward")
+	var up := global_basis.y.normalized()
+	var look_basis := look_pivot.global_basis.orthonormalized()
+	var facing_back := look_basis.z
+	var facing_right := up.cross(facing_back).normalized()
+	facing_back = facing_right.cross(up)
+	var facing_basis := Basis(facing_right, up, facing_back)
+	velocity = facing_basis * move_speed * Vector3(move_vector.x, 0.0, -move_vector.y)
+	move_and_slide()
