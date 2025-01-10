d6900c022235a31bae107ed6d8f6ea1aa9776ab4
Ayla Myers <4943384+aylanonsense@users.noreply.github.com>
2025-01-10 00:19:43 -0500

    Jumping

--- player_character.gd
+++ player_character.gd
@@ -4,24 +4,25 @@ extends CharacterBody3D
 
 @export var move_speed := 5.0
 @export var mouse_look_sensitivity := 1.0
+@export var jump_velocity := 10.0
 
-@onready var look_pivot := %LookPivot as Node3D
+@onready var look_yaw_pivot := %LookYawPivot as Node3D
+@onready var look_pitch_pivot := %LookPitchPivot as Node3D
 
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
-		var look_change := Vector2(event.relative.x, -event.relative.y) * mouse_look_sensitivity
-		look_pivot.rotation_degrees.x = clampf(look_pivot.rotation_degrees.x + look_change.y, -89.9, 89.9)
-		look_pivot.rotation_degrees.y -= look_change.x
+		var look_change := Vector2(event.relative.x, -event.relative.y) * mouse_look_sensitivity # -x = look left / +x = look right / -y = look down / +y = look up
+		look_yaw_pivot.rotation_degrees.y -= look_change.x
+		look_pitch_pivot.rotation_degrees.x = clampf(look_pitch_pivot.rotation_degrees.x + look_change.y, -89.9, 89.9)
 
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	var move_vector := Input.get_vector("move_left", "move_right", "move_backward", "move_forward")
-	var up := global_basis.y.normalized()
-	var look_basis := look_pivot.global_basis.orthonormalized()
-	var facing_back := look_basis.z
-	var facing_right := up.cross(facing_back).normalized()
-	facing_back = facing_right.cross(up)
-	var facing_basis := Basis(facing_right, up, facing_back)
-	velocity = facing_basis * move_speed * Vector3(move_vector.x, 0.0, -move_vector.y) + velocity.y * Vector3.UP
+	velocity = look_yaw_pivot.global_basis * move_speed * Vector3(move_vector.x, 0.0, -move_vector.y) + velocity.y * up_direction
+	velocity += get_gravity() * delta
+	if is_on_floor() and Input.is_action_just_pressed("jump"):
+		velocity.y = jump_velocity
 	move_and_slide()
+	if is_on_floor() and velocity.y < 0.0:
+		velocity.y = 0.0
